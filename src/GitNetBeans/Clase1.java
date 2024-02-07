e869122725db6786014afd5ce86659b8a59d59b9
Ferrokwastaken <israelduquesilva@hotmail.com>
2024-02-07 16:43:16 +0000

    V 0.5 Añadido setters y getters

--- src/GitNetBeans/Clase1.java
+++ src/GitNetBeans/Clase1.java
@@ -9,15 +9,71 @@ package GitNetBeans;
  * @author Daw1
  */
 public class Clase1 {
-    String marca;
-    String matricula;
-    int ruedas;
-    String color;
+    private String marca;
+    private String matricula;
+    private int ruedas;
+    private String color;
 
     public Clase1(String marca, String matricula, int ruedas, String color) {
         this.marca = marca;
         this.matricula = matricula;
         this.ruedas = ruedas;
+        this.color = color;
+    }
+
+    /**
+     * @return the marca
+     */
+    public String getMarca() {
+        return marca;
+    }
+
+    /**
+     * @param marca the marca to set
+     */
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    /**
+     * @return the matricula
+     */
+    public String getMatricula() {
+        return matricula;
+    }
+
+    /**
+     * @param matricula the matricula to set
+     */
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    /**
+     * @return the ruedas
+     */
+    public int getRuedas() {
+        return ruedas;
+    }
+
+    /**
+     * @param ruedas the ruedas to set
+     */
+    public void setRuedas(int ruedas) {
+        this.ruedas = ruedas;
+    }
+
+    /**
+     * @return the color
+     */
+    public String getColor() {
+        return color;
+    }
+
+    /**
+     * @param color the color to set
+     */
+    public void setColor(String color) {
         this.color = color;
     }
     
