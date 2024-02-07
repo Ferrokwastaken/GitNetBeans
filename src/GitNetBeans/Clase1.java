6ecbae9fcfb55764816a49a8f7978036eb0fd811
Ferrokwastaken <israelduquesilva@hotmail.com>
2024-02-07 16:26:37 +0000

    first commit

---
+++ src/GitNetBeans/Clase1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GitNetBeans;
+
+/**
+ *
+ * @author Daw1
+ */
+public class Clase1 {
+    String marca;
+    String matricula;
+    int ruedas;
+    String color;
+
+    public Clase1(String marca, String matricula, int ruedas, String color) {
+        this.marca = marca;
+        this.matricula = matricula;
+        this.ruedas = ruedas;
+        this.color = color;
+    }
+    
+    
+}
---
+++ src/GitNetBeans/Maion.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package GitNetBeans;
+
+/**
+ *
+ * @author Daw1
+ */
+public class Maion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
